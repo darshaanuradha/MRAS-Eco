ac8bec4713c236fef8f95bd73200917642b300b2
Shakila Sandun <Shakilasandun@gmail.com>
2026-07-04 17:00:26 +0530

    Implement requested patient features (search functionality, full details view, and name field indexing)

--- mras/patients/urls.py
+++ mras/patients/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-
-    path('', views.patients_view, name='patients')
+    path('', views.patient_list, name='patients'),
+    path('<int:pk>/', views.patient_detail, name='patient_detail'),
 ]
--- mras/patients/views.py
+++ mras/patients/views.py
@@ -1,4 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Patient
 
-def patients_view(request):
-    return render(request, 'patients.html')
+def patient_list(request):
+    query = request.GET.get('q', '')
+    if query:
+        patients = Patient.objects.filter(name__icontains=query)
+    else:
+        patients = Patient.objects.all()
+    return render(request, 'patients/patient_list.html', {'patients': patients, 'query': query})
+
+def patient_detail(request, pk):
+    patient = get_object_or_404(Patient, pk=pk)
+    return render(request, 'patients/patient_detail.html', {'patient': patient})
