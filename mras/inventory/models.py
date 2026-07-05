6bb0eb1aab369aa5634baa7afd88fd060d426b4a
imlakmali <koralagelal573@gmail.com>
2026-07-05 18:15:52 +0530

    feat: add Medicine model with fields for inventory management

--- mras/inventory/models.py
+++ mras/inventory/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+class Medicine(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    generic_name = models.CharField(max_length=100)
+    manufacturer = models.CharField(max_length=100, blank=True, null=True)
+    dosage_form = models.CharField(max_length=50, blank=True, null=True)
+    strength = models.CharField(max_length=50, blank=True, null=True)
+    min_stock_level = models.IntegerField(default=20)
+    max_stock_level = models.IntegerField(default=500)
+    unit_cost = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        indexes = [
+            models.Index(fields=['name']),
+            models.Index(fields=['generic_name']),
+        ]
+
+    def __str__(self):
+        return self.name    
